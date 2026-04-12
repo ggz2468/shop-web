e9bbd651d2155cd9dc178ad0750f6956da6fb66f
ggz2468 <guiguzi19971006@gmail.com>
2026-04-12 21:57:14 +0800

    更新網站標題

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ const routes = [
     {
         path: '/',
         name: 'Home',
+        meta: { title: `${import.meta.env.VITE_APP_TITLE} - 首頁` },
         component: () => import('@/views/Home.vue'),
     },
     ...productRoutes
@@ -13,6 +14,10 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+})
+
+router.afterEach((to) => {
+    document.title = to.meta.title || import.meta.env.VITE_APP_TITLE
 })
 
 export default router
--- src/router/modules/product.js
+++ src/router/modules/product.js
@@ -2,6 +2,7 @@ export default [
     {
         path: '/products',
         name: 'ProductList',
+        meta: { title: `${import.meta.env.VITE_APP_TITLE} - 產品列表` },
         component: () => import('@/views/product/List.vue'),
     }
 ]
