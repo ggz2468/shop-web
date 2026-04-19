e7b7ff7830f5a42de91d697437047be38b281410
ggz2468 <guiguzi19971006@gmail.com>
2026-04-19 18:08:10 +0800

    [新增] - 新增共用組件

--- src/views/product/List.vue
+++ src/views/product/List.vue
@@ -1,31 +1,7 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import request from '@/utils/request'
-
-const products = ref([])
-
-onMounted(async () => {
-    const response = await request.get('/products')
-    products.value = response.data
-})
+import ProductListTemplate from '@/components/product/ProductListTemplate.vue'
 </script>
 
 <template>
-    <h1>產品列表</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>名稱</th>
-                <th>價格</th>
-                <th>描述</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="product in products" :key="product.id">
-                <td>{{ product.name }}</td>
-                <td>{{ product.price }}</td>
-                <td>{{ product.description }}</td>
-            </tr>
-        </tbody>
-    </table>
+    <ProductListTemplate api-url="/products?row_counts_per_page=15&page=1" />
 </template>
