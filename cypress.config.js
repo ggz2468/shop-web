8fffc09e237214f91b86b8b21729d38a97df292d
ggz2468 <guiguzi19971006@gmail.com>
2026-05-30 20:52:44 +0800

    [新增] - 新增 Cypress 設定檔

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'http://localhost',
+        baseUrl: 'https://chun-hung.idv.tw',
         specPattern: 'tests/e2e/**/*.cy.{js,ts}',
         supportFile: false,
     }
