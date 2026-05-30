03c9c7cfe121418fce0db9a47d5c74c56718a05a
ggz2468 <guiguzi19971006@gmail.com>
2026-05-30 23:23:39 +0800

    [新增] - 新增 Unit Test 與 E2E Test 的範例程式

--- tests/e2e/example.e2e.cy.js
+++ tests/e2e/example.e2e.cy.js
@@ -1,6 +1,6 @@
 describe('首頁 E2E 測試', () => {
   it('應顯示正確標題', () => {
     cy.visit('/')
-    cy.title().should('include', 'shop')
+    cy.title().should('include', Cypress.env('VITE_APP_TITLE'))
   })
 })
